3b71dc761ba0e29ed15e6217bf2362fa0b20d899
Rahma Hussein <88254671+rasamie3@users.noreply.github.com>
2024-05-20 17:20:20 +0300

    Delete create_tabe.sql

--- create_tabe.sql
+++
@@ -1,9 +0,0 @@
-// query to create todo table
-
-
-CREATE TABLE TODO (
-    id SERIAL PRIMARY KEY,
-    task VARCHAR(100), 
-    is_done SMALLINT DEFAULT 0,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
